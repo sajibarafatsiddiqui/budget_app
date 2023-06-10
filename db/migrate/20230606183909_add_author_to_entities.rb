64bf8b824c6883313ab92d55b484e9c59456353b
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-06-10 13:31:30 +0600

    Solved issues

--- db/migrate/20230606183909_add_author_to_entities.rb
+++ db/migrate/20230606183909_add_author_to_entities.rb
@@ -1,6 +1,5 @@
 class AddAuthorToEntities < ActiveRecord::Migration[7.0]
   def change
-    remove_column :deals, :author_id
     add_reference :deals, :user, null: false, foreign_key: true
     rename_column :deals, :user_id, :author_id
   end
