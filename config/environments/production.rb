a6d478551a8fccf4933e84b9adfbad9058de1255
sajibarafatsiddiqui <siddiquisajib75@gmail.com>
2023-06-11 00:12:35 +0600

    Changed in puma.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,8 +5,6 @@ require 'active_support/core_ext/integer/time'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
-  url = blob.service_url_for_direct_upload(host: 'myhost.com')
 
   config.cache_classes = true
 
